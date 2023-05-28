89c8d7ea21e286a3cb23c76aa8ccef9ec09149f7
Foxglade <kapu.kettu@gmail.com>
2023-05-28 20:01:48 +0300

    update

--- Ajatuspilvi/app/src/main/java/com/example/ajatuspilvi/MainActivity.kt
+++ Ajatuspilvi/app/src/main/java/com/example/ajatuspilvi/MainActivity.kt
@@ -1,45 +1,8 @@
 package com.example.ajatuspilvi
-import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var chatView: TextView
-    private lateinit var messageEditText: EditText
-    private lateinit var sendButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
-        chatView = findViewById(R.id.chatView)
-        messageEditText = findViewById(R.id.messageEditText)
-        sendButton = findViewById(R.id.sendButton)
-
-        sendButton.setOnClickListener {
-            val message = messageEditText.text.toString()
-            if (message.isNotEmpty()) {
-                addMessage(message)
-                messageEditText.text.clear()
-            }
-        }
-    }
-
-    private fun addMessage(message: String) {
-        val newMessage = TextView(this)
-        newMessage.text = message
-        chatView.addView(newMessage)
-    }
 }
 
 
